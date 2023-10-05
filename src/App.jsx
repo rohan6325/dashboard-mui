82cd67a3c2036593c3b4df111bc6cccea55e91cf
Rohan Sharma <sharma.rs@somaiya.edu>
2023-10-05 19:16:41 +0530

    calorie and line graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import Sidebar from './components/Sidebar'
+import FitnessDataVisualization from './components/Grid';
+
 
 export default function App() {
+ 
+
+
   return (
+   
     <>
       <Sidebar/>
     </>
-
+    
+   
   );
 }
---
+++ src/components/Calories.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function CalorieBurnInfographic() {
+    const [caloriesBurned, setCaloriesBurned] = useState(0);
+    const maxCalories = 1000; // Updated maximum calories to 1000
+  
+    const handleCaloriesChange = (event) => {
+      setCaloriesBurned(event.target.value);
+    };
+  
+    // Calculate the size of the emoji symbol based on calories burned
+    const emojiSize = `${48 + (caloriesBurned / maxCalories) * 48}px`; // Larger initial size and growth
+  
+    return (
+      <div>
+        <Typography variant="h5" gutterBottom>
+          Calories Burned: {caloriesBurned} kcal
+        </Typography>
+        <EmojiSymbol size={emojiSize}>🔥</EmojiSymbol>
+        <FormControl>
+          <Select
+            value={caloriesBurned}
+            onChange={handleCaloriesChange}
+            variant="outlined"
+            displayEmpty
+          >
+            <MenuItem value={0}>Select Calories Burned</MenuItem>
+            <MenuItem value={50}>50 kcal</MenuItem>
+            <MenuItem value={100}>100 kcal</MenuItem>
+            <MenuItem value={1000}>1000 kcal</MenuItem>
+    
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
