1ed7f00418936037b71ce39d4d9e602bb16b7354
Rohan Sharma <sharma.rs@somaiya.edu>
2023-10-18 22:02:47 +0530

    grid update

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -30,7 +30,9 @@ const Item = styled(Paper)(({ theme }) => ({
   justifyContent: 'center',
   width: '100%',
   height: '100%',
+  boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)', // Adjust the values for a 3D effect
 }));
+
 
 export default function FitnessDataVisualization() {
   const [pieChartDataIndex, setPieChartDataIndex] = useState(0);
